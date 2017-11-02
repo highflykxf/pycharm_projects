c7c560590ff0f17f473a593e367c3f59a967e194
highfly <xiangfei0919@gmail.com>
2017-11-02 23:09:25 +0800

    add event-registry-python-master

---
+++ test/event-registry-python-master/EventRegistry/examples/QueryEventExamples.py
@@ -0,0 +1,20 @@
+﻿from eventregistry import *
+
+er = EventRegistry()
+
+# get info about event with a particular uri
+q = QueryEvent("eng-88")
+q.addRequestedResult(RequestEventInfo(
+    returnInfo = ReturnInfo(conceptInfo = ConceptInfoFlags(lang = ["eng", "spa", "slv"]))))
+q.addRequestedResult(RequestEventArticles(page = 1, count = 10))        # get 10 articles about the event (any language is ok) that are closest to the center of the event
+q.addRequestedResult(RequestEventArticleTrend())
+q.addRequestedResult(RequestEventKeywordAggr())
+eventRes = er.execQuery(q)
+
+# obtain in one call information about events with uris 234, 212 and 423
+q = QueryEvent(["spa-32", "spa-45"])
+q.addRequestedResult(RequestEventArticles(page =1, count = 200))
+res = er.execQuery(q)
+obj = createStructFromDict(res)
+
+
